d4c1b94436bb8ee4592c3b7230d3f793f50ef1bc
Beshoy S. Said <72396606+BeshoyS@users.noreply.github.com>
2022-02-12 11:58:10 +0100

    make it better

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const dbConn = require('./config/config');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 const customerRoutes = require('./src/customers/routes/customerRoutes');
 
-
 dbConn();
-app.use(express.json())
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => res.send('Hello World!'));
 app.use(customerRoutes);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
